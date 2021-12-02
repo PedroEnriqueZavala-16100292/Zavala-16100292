b86861c42b9e254c584f90f4bd2ba45e03f9e352
PedroEnriquezavala-16100292 <L16100292@nlaredo.tecnm.mx>
2021-12-02 17:23:56 -0600

    original

---
+++ Parcial1/Parcial3/PhpPDO/conectar.php
@@ -0,0 +1,18 @@
+<?php
+
+$hostname='localhost';
+$database='Web16100292';
+$username='PEDRO-ZAVALA\SQLANALISIS';
+$password='';
+
+try {
+      $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
+} catch(PDOException $e) {
+      echo "Error de conexion a la base de datos";
+      echo $e->getMessage();
+      exit();
+}
+
+$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+?>
