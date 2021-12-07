2c78579a5332b3c11dc08493d95599a8f486bb69
PedroEnriquezavala-16100292 <L16100292@nlaredo.tecnm.mx>
2021-12-07 00:43:33 -0600

    x

---
+++ Parcial3/LoginFijo/js/index.js
@@ -0,0 +1,12 @@
+$('document').ready(function () {
+
+    $('#botonSalir').click(function () {
+        $('#modalSalida').modal();
+    });
+
+     $("#botonMenu").click(function (e) {
+        e.preventDefault();
+        $("#contenedorPrin").toggleClass("toggled");
+    });
+
+});
