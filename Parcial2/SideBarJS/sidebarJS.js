c99ed7e2ae92d7cc5c2dcdaacbcec78b4946490c
PedroEnriqueZavala-16100292 <l16100292@nlaredo.tecnm.mx>
2022-10-18 05:31:09 -0700

    act

---
+++ Parcial2/SideBarJS/sidebarJS.js
@@ -0,0 +1,18 @@
+window.onload=function(){
+    
+    document.getElementById('btn-menu').addEventListener('click',function()  {
+       document.getElementById('navegador').classList.toggle('active');
+        console.log('desplegar y esconder con Javascript');
+    })
+
+   /* document.getElementById("btn-menu").addEventListener("click",()=> {
+        document.getElementById("btn-menu").addEventListener()
+    }) 
+
+    
+    const sidebar = document.querySelectorAll('.toogle-btn');
+
+sidebar.addEventListener('click', function () {
+        document.getElementById('container').classList.toggle('active');
+    });*/
+}
