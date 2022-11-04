88f1348ea19b1210d99e21e37bbcb2709155cfb2
PedroEnriqueZavala-16100292 <l16100292@nlaredo.tecnm.mx>
2022-11-03 19:21:02 -0700

    act

--- Parcial2/AsyncAwait/Asincwait.js
+++ Parcial2/AsyncAwait/Asincwait.js
@@ -1,46 +1,32 @@
-const marvel = {
-    render: () => {
-      const urlAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=cefbd57bd9ea6aeb058a0839896f197f&hash=90c9ddb41876bb88468c5b7abad42378';
-      const container = document.querySelector('#marvel-row');
-      let contentHTML = '';
-  
-      fetch(urlAPI)
-        .then(res => res.json())
-        .then((json) => {
-          for (const hero of json.data.results) {
-            let urlHero = hero.urls[0].url;
-            contentHTML += `
-              <div class="col-md-4">
-                  <a href="${urlHero}" target="_blank">
-                    <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
-                  </a>
-                  <h3 class="title">${hero.name}</h3>
-              </div>`;
-          }
-          container.innerHTML = contentHTML;
-        })
-    }
-  };
-  marvel.render();
-
-  document.getElementById("btnPeticionFecth").addEventListener("click",function() {
-    fetch(urlAPI)
-    .then(respuesta => respuesta.json() )
-    .then(dataJson => document.getElementById("caja").innerText=dataJson,value);
-})
+const urlAPI =
+  "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=cefbd57bd9ea6aeb058a0839896f197f&hash=90c9ddb41876bb88468c5b7abad42378";
 
 
-document.getElementById("btnPeticionJquery".addEventListener("click",async function() {
-let respuesta = await fetch(urlAPI);
-let dataJson= await respuesta.json();
 
-document.getElementById("caja").innerText=dataJson.value;
-}))
+async function myQuery() {
+  const {data} = await (await fetch(urlAPI)).json();
+  const {results} = data
+  console.log(results);
 
-$(document).ready(function() {
-document.getElementById("btnJquerys").addEventListener("click",function() {
-    $.getJSON(urlAPI),function(dataJson) {
-      ${"#marvel-row"}.then(dataJson.value);
-    }
-})
-})
+  results.forEach(item => {
+    makeCard(item.name,item.thumbnail.path,item.thumbnail.extension)
+  });
+
+}
+
+function makeCard(title,content,ext) {
+  const card = `
+  <div class="col-md-4 mt-3">
+  <div class="card">
+    <h2 class="card-header">${title}</h2>
+    <div class="card-body">
+    <img class="img-thumbnail" src="${content}.${ext}">
+    </div>
+  </div>
+</div>`;
+
+document.getElementById('marvel-row').innerHTML += card;
+
+}
+
+myQuery();
