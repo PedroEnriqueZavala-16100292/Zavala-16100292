def19d43a1848fc11b44bdd7f0b9b94a2db63498
PedroEnriqueZavala-16100292 <l16100292@nlaredo.tecnm.mx>
2022-12-02 18:44:48 -0600

    act

--- Parcial3/Grid/Consulta.php
+++ Parcial3/Grid/Consulta.php
@@ -3,9 +3,9 @@ $servidor = "localhost";
 $basedatos = "recetasmedicas";
 $usuario = "root";
 $password = "";
-$port = "3308";
 
-$con = mysqli_connect($servidor,$usuario,$password,$basedatos,$port) or die("No se pudo conectar al servidor");
+
+$con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar al servidor");
 $consulta = "select * from recetaspaci";
 $registros = mysqli_query($con,$consulta) or die ("problemas si");
 /*die($consulta);*/
